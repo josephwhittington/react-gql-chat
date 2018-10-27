3b1754a2563d6048846d2f9deb93d14b4dc6269b
Joseph Whittington <joseph.a.whittington@gmail.com>
2018-10-26 23:29:56 -0400

    set up transport for websockets to support gql subscriptions

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,45 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { ApolloProvider } from "react-apollo";
-import ApolloClient from "apollo-boost";
+import { InMemoryCache, ApolloClient } from "apollo-boost";
+import { WebSocketLink } from "apollo-link-ws";
+import { split } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import { getMainDefinition } from "apollo-utilities";
 
 import App from "./App";
 
 import store from "./store";
 
-const client = new ApolloClient({ uri: "http://localhost:4000/graphql" });
+// Create an http link:
+const httpLink = new HttpLink({
+    uri: "http://localhost:4000/graphql"
+});
+
+// Create a WebSocket link:
+const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000/subscription`,
+    options: {
+        reconnect: true
+    }
+});
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+    // split based on operation type
+    ({ query }) => {
+        const { kind, operation } = getMainDefinition(query);
+        return kind === "OperationDefinition" && operation === "subscription";
+    },
+    wsLink,
+    httpLink
+);
+
+const client = new ApolloClient({
+    link,
+    cache: new InMemoryCache()
+});
 
 ReactDOM.render(
     <Provider store={store}>
